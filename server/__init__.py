19fd54f6f85df3774ab593cda846de25424d9751
Aleksey Kaptur <102624582+rubybo@users.noreply.github.com>
2023-01-20 17:33:33 +0300

    Landing page

---
+++ server/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mainsql.db'
+app.config['SECRET_KEY'] = "kfsfee23492193sjhfehgaawge344451990"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+from server import route
